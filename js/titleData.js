f65f068205f00f30c671f709af29f6eb7a457924
Moonshine <710159940@qq.com>
2018-12-29 15:37:21 +0800

    1、基本信息修改为动态添加

--- js/titleData.js
+++ js/titleData.js
@@ -4,27 +4,13 @@ setInterval(function () {
     var text = document.getElementById('user');
     text.innerText =data;
 },2000);
-setInterval(function () {
-    const hour =  (Math.random() * 100).toFixed(0) - 0;
-    const min =  Math.abs((Math.random() * 100).toFixed(0) - 60);
-    var time = document.getElementById('userTime');
-    time.innerText =hour+'小时'+min+'分钟';
-},2000);
 //右边车流量
 setInterval(function () {
     const data =  (Math.random() * 1000).toFixed(0) - 0;
     var car = document.getElementById('car');
     car.innerText =data;
 },2000);
-setInterval(function () {
-    const hour =  (Math.random() * 100).toFixed(0) - 0;
-    const min =  Math.abs((Math.random() * 100).toFixed(0) - 60);
-    var time = document.getElementById('carTime');
-    time.innerText =hour+'小时'+min+'分钟';
-},2000);
 
-
-// vehicle();
 var time = document.getElementById("time");
 var date = document.getElementById("date");
 var nowWeek = document.getElementById("nowWeek");
@@ -38,7 +24,6 @@ function displayTime() {
     var timestr = year + '/' + month + '/' + day;
     date.innerHTML= timestr;
     nowWeek.innerHTML = weekday[week];
-    // document.write("今天是：" + );
 }
 function clock(){
     let nowTime = new Date();
