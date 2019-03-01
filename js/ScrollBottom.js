4072ee1e30bb8c57d2f323cff0a6ad0f30cadb8b
Moonshine <710159940@qq.com>
2019-03-01 15:41:47 +0800

    操作日志：     (1)、修改轮播图的显示方式

--- js/ScrollBottom.js
+++ js/ScrollBottom.js
@@ -20,26 +20,7 @@ var divoption ='';
 // </div>
 divoption = `<div class="bottom">
     <div class="bottomDiv" id="div">
-       <div id="banner">
-        <div id="banner_bg">
-        </div>
-        <div id="banner_info">
-        </div>
-        <ul>
-            <li class="on"> 1 </li>
-            <li> 2 </li>
-            <li> 3 </li>
-            <li> 4 </li>
-            <li> 5 </li>
-        </ul>
-        <div id="banner_list">
-            <a href="#" target="_blank"><img src="../lib/image/1-1.jpg"></a>
-            <a href="#" target="_blank"><img src="../lib/image/1-2.jpg"></a>
-            <a href="#" target="_blank"><img src="../lib/image/1-3.jpg"></a>
-            <a href="#" target="_blank"><img src="../lib/image/1-4.jpg"></a>
-            <a href="#" target="_blank"><img src="../lib/image/1-5.jpg"></a>
-        </div>
-    </div>
+     <img src="../lib/image/1-1.jpg">
     </div>
     <div class="lt all"></div>
     <div class="rt all"></div>
@@ -96,37 +77,3 @@ $('body').append(divoption);
 //         }
 //     }
 // }
-var t = n = 0,
-    count;
-$(document).ready(function() {
-    // 获取图片标签长度
-    count = $("#banner_list a").length;
-    // 不是当前显示的图片隐藏
-    $("#banner_list a:not(:first-child)").hide();
-    // 点击下面的1234按钮，切换图片
-    $("#banner li").click(function() {
-        var i = $(this).text() - 1;
-        n = i;
-        if (i >= count) return;
-        // 淡入淡出效果
-        $("#banner_list a").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
-
-        // 响应点击
-        $(this).toggleClass("on");
-
-        // 切换按钮的时候让上一个按钮回归原来的样式
-        $(this).siblings().removeAttr("class");
-    });
-    t = setInterval("showAuto()", 4000);
-    $("#banner").hover(function() {
-            clearInterval(t)
-        },
-        function() {
-            t = setInterval("showAuto()", 4000);
-        });
-})
-// 自动轮播
-function showAuto() {
-    n = n >= (count - 1) ? 0 : ++n;
-    $("#banner li").eq(n).trigger('click');
-}
