e293b7d99cda8692164b0970ee87a9be4c385032
Moonshine <710159940@qq.com>
2019-03-05 19:34:13 +0800

    操作日志：     (1)、修改轮播图的显示方式     (2)、修改视频上下切换

--- js/ScrollBottom.js
+++ js/ScrollBottom.js
@@ -1,11 +1,11 @@
-var arr =new Array();
-arr[0] = "贵州省贵阳市修文县久长镇，位于修文、开阳、息烽三县交界" +
-    "贵阳市北，修文县城东北，距县城约15公里，距省城贵阳约45公里。是开阳、息烽两县进入省城的必经之地。" +
-    "全镇总面积118多平方公里，耕地3.2万亩，其中，田1.55万亩，土1.64万亩。" +
-    "镇辖22个村、1个居委会，人口31759人。修文县辖镇。位于县境东北部。辖1个社区、22个行政村。镇政府驻久长坝，距县城17千米。" +
-    "川黔铁路、210国道和G75兰海高速公路并行于镇中部。";
-
-var divoption ='';
+// var arr =new Array();
+// arr[0] = "贵州省贵阳市修文县久长镇，位于修文、开阳、息烽三县交界" +
+//     "贵阳市北，修文县城东北，距县城约15公里，距省城贵阳约45公里。是开阳、息烽两县进入省城的必经之地。" +
+//     "全镇总面积118多平方公里，耕地3.2万亩，其中，田1.55万亩，土1.64万亩。" +
+//     "镇辖22个村、1个居委会，人口31759人。修文县辖镇。位于县境东北部。辖1个社区、22个行政村。镇政府驻久长坝，距县城17千米。" +
+//     "川黔铁路、210国道和G75兰海高速公路并行于镇中部。";
+//
+// var divoption ='';
 // divoption = `<div class="bottom">
 //     <div class="bottomDiv" id="div">
 //       <ul id="ul">
@@ -18,18 +18,8 @@ var divoption ='';
 //     <div class="lb all"></div>
 //     <div class="rb all"></div>
 // </div>
-divoption = `<div class="bottom">
-    <div class="bottomDiv" id="div" style="text-align: center">
-     贵州高投集团
-    </div>
-    <div class="lt all"></div>
-    <div class="rt all"></div>
-    <div class="lb all"></div>
-    <div class="rb all"></div>
-</div>
-<!--设置底部的版权信息-->
-<div class="bottomtext" >版权所有：贵州高投服务管理有限公司 <span>123</span> 技术支持：贵阳红鸟智能技术服务有限公司</div>`;
-$('body').append(divoption);
+// divoption = ``;
+// $('body').append(divoption);
 //图文信息模板
 
 
--- js/videos.js
+++ js/videos.js
@@ -5,6 +5,30 @@ var video = document.getElementById('video');
 video.addEventListener('ended', play);
 play();
 
+console.log(video);
+// 左边点击切换视频
+function leftimgclick() {
+    if (curr < 1){
+        // console.log('12');
+        alert('已经是第一个视频');
+    }else {
+        curr--;
+        console.log(curr);
+        playVide();
+    }
+}
+// 右边点击切换视频
+function rightimgclick() {
+    // console.log('34');
+    if (curr >= video_length-1){
+      alert('已经是最后一个视频');
+    }else {
+        curr++;
+        console.log(curr);
+        playVide();
+    }
+
+}
 function play() {
     console.log(video_list[curr]);
     video.src = video_list[curr];
@@ -14,4 +38,10 @@ function play() {
     if (curr >= video_length) {
         curr = 0;//播放完后，重新播放
     }
+}
+function playVide() {
+    console.log(video_list[curr]);
+    video.src = video_list[curr];
+    video.load();//若视频很短，加载完后再播放，监听canplaythrough事件即可
+    video.play();
 }
